12bdb0e1fb1cac2320c137884a9bb40ada890dba
XinZhao92 <cynthia_z92@outlook.com>
2017-05-12 13:08:24 +0200

    Clean up some not related file.

--- 2016/callgraphs/src/de/tudarmstadt/apsa/callgraphs/examples/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package de.tudarmstadt.apsa.callgraphs.examples;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
-}
--- 2016/llvm/examples/factorial.c
+++
@@ -1,8 +0,0 @@
-int factorial (int n) {
-	int r = 1;
-	while (n > 0) {
-		r *= n;
-		n--;
-	}
-	return r;
-}
